da7aceaf2ce5fafff2d605bb136f9439dcaa7c33
MikeDePacina <mikedpacina@gmail.com>
2023-07-09 00:07:33 -0700

    imported and using Gorilla mux router instead

--- api/api.go
+++ api/api.go
@@ -3,12 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
-   func helloGoHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("I love Melissa!\n"))
-   }
-   func main() {
-	http.HandleFunc("/", helloGoHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-   }
+   
+func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello gorilla/mux!\n"))
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Step 2: Register the route with the handler function
+	r.HandleFunc("/", helloMuxHandler)
+
+	// Step 3: Start the HTTP server
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
    
